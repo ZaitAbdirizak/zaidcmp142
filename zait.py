7c3cea7389fb689b54a06113ebdf43d988ac021f
ZaitAbdirizak <k20230815@std.kyrenia.edu.tr>
2025-11-11 14:56:55 +0200

    Update zait.py

--- zait.py
+++ zait.py
@@ -1,5 +1,17 @@
-class Animal:
-  def speak(self):
-    print("Animal speaking")
+#this project is for Library management system
+
+class Book:
+  def __init__(self, title, author):
+    self.title = "python guide"
+    self.author = "jake"
+
+f1 = Books()
+f1.title = "python guide"
+f1.author = "jake"
+print(f1.title)
+print(f1.author)
+
+    
+
 
 
